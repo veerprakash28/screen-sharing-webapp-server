f5ec89a75516e0a5fa74572f285ac055cb2184fc
Veer Prakash <veer.prakash_cs.aiml19@gla.ac.in>
2024-02-02 01:10:42 +0530

    added endpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:5001",
+    origin: "https://screen-sharing-webapp.netlify.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   },
